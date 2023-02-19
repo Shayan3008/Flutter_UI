a422ae0e3148495334f935d53da732f6f493bea5
shayan <sb15@mmcgbl.com>
2023-02-19 23:01:26 +0500

    Home Screen Ui Done

---
+++ dribbleui/lib/screens/HomeScreen.dart
@@ -0,0 +1,102 @@
+import 'package:dribbleui/configs/SizeConfig.dart';
+import 'package:dribbleui/configs/StyleConfig.dart';
+import 'package:dribbleui/widgets/BoldText.dart';
+import 'package:dribbleui/widgets/GridContainersWidget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    List<Color> list = [
+      Colors.blue,
+      Colors.orange,
+      Colors.red,
+      Colors.yellow,
+    ];
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 25,
+            vertical: 25,
+          ),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                BoldTextWidget(text: 'Your Dribbbox'),
+                Icon(
+                  Icons.menu,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: (SizeConfig.screenHeight! / 100) * 4,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(width: 1, color: Colors.grey),
+                ),
+                hintText: 'Search Folder',
+                hintStyle: kQuestrialMedium,
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: SizeConfig.horizantalAspect! * 7,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: (SizeConfig.screenHeight! / 100) * 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Recent',
+                      style: kQuestrialSemiBold.copyWith(
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      width: SizeConfig.horizantalAspect! * 2,
+                    ),
+                    Icon(
+                      Icons.arrow_downward,
+                      size: SizeConfig.horizantalAspect! * 4,
+                    ),
+                  ],
+                ),
+                const Icon(Icons.filter_alt)
+              ],
+            ),
+            SizedBox(
+              height: (SizeConfig.screenHeight! / 100) * 2,
+            ),
+            GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 1,
+                  crossAxisCount: 2,
+                  childAspectRatio: 21 / 16,
+                ),
+                itemCount: 4,
+                itemBuilder: ((context, index) => GridContainersWidget(
+                      color: list[index],
+                      image: 'assets/GridImage.svg',
+                      description: 'Mobile Apps',
+                      subDescription: 'December 20.2020',
+                    )))
+          ]),
+        ),
+      ),
+    );
+  }
+}
--- dribbleui/lib/widgets/GridContainersWidget.dart
+++ dribbleui/lib/widgets/GridContainersWidget.dart
@@ -1,17 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class MyWidget extends StatelessWidget {
+import '../configs/SizeConfig.dart';
+import '../configs/StyleConfig.dart';
+import 'BoldText.dart';
+
+class GridContainersWidget extends StatelessWidget {
+  final Color color;
   final String image;
   final String description;
   final String subDescription;
-  const MyWidget(
+  const GridContainersWidget(
       {super.key,
       required this.image,
       required this.description,
-      required this.subDescription});
+      required this.subDescription,
+      required this.color});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: color.withOpacity(0.2),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: SizeConfig.horizantalAspect! * 1.5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset(
+                    image,
+                    color: Colors.orange,
+                  ),
+                  Image.asset(
+                    'assets/IconForGrid.png',
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: SizeConfig.verticalAspect! * 1,
+              ),
+              BoldTextWidget(
+                text: description,
+                color: Colors.orange,
+              ),
+              SizedBox(
+                height: SizeConfig.verticalAspect! * 1,
+              ),
+              Text(
+                subDescription,
+                style: kQuestrialSemiBold.copyWith(
+                  color: Colors.orange,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
